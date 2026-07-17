ac6750bef377ce4b52d3323972ffeb93c3a9d9e4
thethr0ne7 <124492332+thethr0ne7@users.noreply.github.com>
2026-07-17 16:25:22 +0300

    style: load gameplay depth feedback styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './styles.css'
 import './chain.css'
+import './gameplay-depth.css'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
